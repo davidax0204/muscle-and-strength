e37b37b1459d8930bc33424402bd8e689348ed15
davidax0204 <71772975+davidax0204@users.noreply.github.com>
2022-09-29 11:32:09 +0300

    progress

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,47 @@
 const btn = document.getElementById("test");
 
-
-
 btn.addEventListener("click",async ()=>{
   const [tab] = await chrome.tabs.query({active:true, currentWindow:true});
   chrome.scripting.executeScript({
       target:{tabId:tab.id},
       func:()=>{
-          console.log('working')
+
+        var viewMoreButton = document.getElementsByClassName("view-more")[0]
+
+        if(viewMoreButton.getAttribute("style")!='display: none;'){
+            viewMoreButton.click()
+        }
+
+        var interval = setInterval(()=>{
+            viewMoreButton = document.getElementsByClassName("view-more")[0]
+        
+            if(viewMoreButton.getAttribute("style")!='display: none;'){
+                viewMoreButton.click()
+            }
+            else{
+                var ankers = document.getElementsByClassName("image-wrap box-image")
+
+                Array.prototype.forEach.call(ankers, child => {
+                    if(child.getAttribute("title").includes("(Exp"))
+                    child.parentNode.parentNode.remove()
+                  });
+
+                clearInterval(interval)
+            }
+        },1000)
+
+
+
+        //   console.log(viewMoreButton)
           const res = 1;
           return res;
       }
   }).then((result)=>{
-      result[0].result; // = 1
+    //   result[0].result; //
   })
 })
+
+function getExpiredProducts(){
+    var ankers = document.getElementsByClassName("image-wrap box-image")
+    console.log(ankers)
+}
